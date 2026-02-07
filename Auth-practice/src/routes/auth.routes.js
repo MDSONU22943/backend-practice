f1820e0d3b02b2aa9d3d310288efa0c8afa5367c
MDSONU22943 <mdsonu22943@gmail.com>
2026-02-07 11:22:56 +0530

    add auth and jwt

--- Auth-practice/src/routes/auth.routes.js
+++ Auth-practice/src/routes/auth.routes.js
@@ -5,5 +5,16 @@ const router = express.Router()
 
 router.post('/register', authController.registerUser)
 
+router.get('/test', (req,res)=>{
+    console.log("cookies:", req.cookies);
+    res.status(200).json({message: 'Test route accessed successfully',
+        cookies: req.cookies
+    })
+
+
+})
+
+
+
 
 module.exports = router
